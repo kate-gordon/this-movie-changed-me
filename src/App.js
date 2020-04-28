5270b3ade03e99538901d92aa37cd5b1f30f6103
Kate Gordon <kategordon.code@gmail.com>
2020-04-28 16:40:45 -0400

    set up react router dom in App js nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Main from "./pages/Main";
 import MovieSearch from "./pages/MovieSearch";
 import Navbar from "./components/Navbar";
@@ -6,8 +8,33 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className='App'>
-      <Navbar />
-      <Main />
+      <Router>
+        <div>
+          <ul
+            style={{
+              listStyleType: "none",
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <li style={{ margin: "5px", padding: "5px" }}>
+              <Link to='/'>Home</Link>
+            </li>
+            <li style={{ margin: "5px", padding: "5px" }}>
+              <Link to='/experience'>The Experience</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path='/experience'>
+              <MovieSearch />
+            </Route>
+            <Route path='/'>
+              <Main />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
