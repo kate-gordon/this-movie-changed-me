93e7e6a5d077487422aed0d54bfb8c6be102d424
Kate Gordon <kategordon.code@gmail.com>
2020-04-28 17:27:12 -0400

    added usestate hook to ResultsGrid to store users selection from their search results

--- src/components/ResultsGrid.jsx
+++ src/components/ResultsGrid.jsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Tile from "../components/Tile";
 import Button from "../components/Button";
 
 const ResultsGrid = (props) => {
+  const [selMovie, setSelectedMovie] = useState();
+
   let arr = () => {
     if (props.results === undefined) {
       return [];
     } else return props.results.results;
   };
 
-  const handleClick = (e) => {
-    console.log("clicked");
-  };
-
   const resultArr = arr();
 
-  let movieResults = resultArr.map(function (movie) {
+  let movieResults = resultArr.map(function (movie, index) {
     const srcLink = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
     return (
       <Tile key={movie.id} title={movie.title}>
@@ -25,13 +23,21 @@ const ResultsGrid = (props) => {
           <h2>Image Unavailable</h2>
         )}
         {movie.title}
-        <Button onClick={handleClick} label='Select' />
+        <Button
+          onClick={() => setSelectedMovie(props.results.results[index])}
+          label='Select'
+        />
       </Tile>
     );
   });
 
   return (
     <>
+      {selMovie !== undefined ? (
+        <h2>{selMovie.title}</h2>
+      ) : (
+        <h2> No Movie Selected</h2>
+      )}
       <ul
         style={{
           listStyleType: "none",
